bd86c8319fa48828685ff5dac77b3346b6487f51
Michal-MK <michalhz159@gmail.com>
2019-08-24 12:06:11 +0200

    Added option to space out lines (improves readability for manual config editing) - Header now detectes whether to prepend a '# '

--- Igor.Configuration/Extensions/CommentAttribute.cs
+++ Igor.Configuration/Extensions/CommentAttribute.cs
@@ -4,9 +4,11 @@ namespace Igor.Configuration {
 	public class CommentAttribute : Attribute {
 
 		internal string Comment { get; set; }
+		internal bool NewLine { get; set; }
 
-		public CommentAttribute(string comment = "") {
+		public CommentAttribute(string comment = "", bool offsetWithNewLine = true) {
 			Comment = string.IsNullOrWhiteSpace(comment) ? "" : comment;
+			NewLine = offsetWithNewLine;
 		}
 	}
 }
